60472377d6d91f5d65725b6d9787e7e4c370dce6
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-11 00:57:57 +0530

    Commit#150

--- src/app/odd/odd.component.ts
+++ src/app/odd/odd.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./odd.component.css']
 })
 export class OddComponent implements OnInit {
-  oddNumbers = []; // array to store odd numbers
-  evenNumbers = []; // array to store even numbers
+  oddNumbers: number[] = []; // array to store odd numbers
+  evenNumbers: number[] = []; // array to store even numbers	
 
   @Input() number: number;
   constructor() { }
