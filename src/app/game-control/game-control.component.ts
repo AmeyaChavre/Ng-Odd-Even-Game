2825de602bcf414ff12069f0711e86d9676c7a3b
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-09-10 22:41:39 +0530

    Commit#35

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -15,7 +15,7 @@ export class GameControlComponent implements OnInit {
   }
 
   onGameStart(){
-  	this.interval = setInterval(1000);
+  	this.interval = setInterval(() => {}, 1000);
   }
 
   onGamePause(){
